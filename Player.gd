d39daf7d28724915ee1f94ca3a5f9aabcde3e85f
Sean Allen <angstyloop@gmail.com>
2023-05-27 15:52:53 -0700

    removed unused bits of code

--- Player.gd
+++ Player.gd
@@ -11,7 +11,6 @@ const initial_acceleration = 5000
 const acceleration_growth = 100
 const drag = 10
 const stopping_speed = 250
-#var angular_speed = PI
 
 func _init():
     var size = OS.get_real_window_size()
@@ -36,20 +35,7 @@ func move(direction_index, delta):
             speed[direction_index]  = speed_limit
     
 func _process(delta):
-    #var direction = 0    
-    
-    if Input.is_action_just_pressed("ui_up"):
-        direction = Vector2.UP
-    
-    if Input.is_action_just_pressed("ui_right"):
-        direction = Vector2.RIGHT
-        
-    if Input.is_action_just_pressed("ui_down"):
-        direction = Vector2.DOWN
-            
-    if Input.is_action_just_pressed("ui_left"):
-        direction = Vector2.LEFT
-    
+  
     if Input.is_action_pressed("ui_up"):
         move(0, delta)
     
@@ -59,10 +45,7 @@ func _process(delta):
     if Input.is_action_pressed("ui_down"):
         move(2, delta)
 
-    #rotation += angular_speed * direction * delta
-    
     if Input.is_action_pressed("ui_left"):
-        #velocity = Vector2.UP.rotated(rotation) * speed
         move(3, delta)
     
     if Input.is_action_pressed("ui_home"):

b68eba110ad85f1d81a7b893b304345e47a0ae97
Sean Allen <angstyloop@gmail.com>
2023-05-28 03:42:07 -0700

    coupled movement stats to soul

--- Player.gd
+++ Player.gd
@@ -1,35 +1,87 @@
 extends Area2D
 
+var window_size
 var speed
-var spin_speed = 12
+var spin_speed
 var direction
 var speed_sum
 var soul
-const len_soul = 4
-const initial_speed = 1
-const speed_limit = 10000
-const initial_acceleration = 5000
-const acceleration_growth = 100
-const directions = [Vector2.UP, Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT]
+var initial_speed
+var speed_limit
+var initial_acceleration
+var acceleration_growth
 var drag
-const stopping_speed = 250
+var stopping_speed
+
+var health
+var armor
+
+const directions = [Vector2.UP, Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT]
+const len_soul = 4
+const base_drag = 4
 
 signal soul_switch(soul)
 
 func _init():
-    var size = OS.get_real_window_size()
-    position = Vector2(size[0] / 2, size[1] / 2)
+    window_size = OS.get_real_window_size()
+    position = Vector2(window_size[0] / 2, window_size[1] / 2)
     
+    soul = [0, 0, 0, 0]    
     speed = [0, 0, 0, 0]
+    
     direction = Vector2.ZERO
-    soul = [0, 0, 0, 0]
-    drag = 10
+    
+    update_stats(soul)
+
+func update_stats(soul):
+    spin_speed = get_spin_speed(soul)
+    drag = get_drag(soul)
+    initial_speed = get_initial_speed(soul)
+    speed_limit = get_speed_limit(soul)
+    initial_acceleration = get_initial_acceleration(soul)
+    acceleration_growth = get_acceleration_growth(soul)
+    stopping_speed = get_stopping_speed(soul)
+    
+static func get_soul_component(soul, i):
+    var sum = 0
+    for j in soul:
+        if j == i:
+            sum += 1
+    return sum
+
+static func get_spin_speed(soul):
+    return 4 * get_soul_component(soul, 3)
+
+static func get_drag(soul):
+    return base_drag + 3 * get_soul_component(soul, 2)
+
+static func get_initial_speed(soul):
+    return 1 * get_soul_component(soul, 3)
+
+static func get_speed_limit(soul):
+    return 2500 * get_soul_component(soul, 3)
+
+static func get_initial_acceleration(soul):
+    return 1250 * get_soul_component(soul, 3)
+
+static func get_acceleration_growth(soul):
+    return 25 * get_soul_component(soul, 3)
+
+static func get_armor(soul):
+    return 1 * get_soul_component(soul, 2)
+    
+static func get_regen(soul):
+    return 1 * get_soul_component(soul, 1)
+
+static func get_stopping_speed(soul):
+    return 62.5 * get_soul_component(soul, 3)
 
 func soul_push_back(soul_index):
     for i in len_soul - 1:
         soul[i] = soul[i + 1]
     soul[len_soul - 1] = soul_index
     emit_signal("soul_switch", soul)
+    update_stats(soul)  
         
 func move(direction_index, delta):
     if (Vector2.ZERO - direction).length() < 0.000001:
@@ -47,19 +99,15 @@ func move(direction_index, delta):
 func _process(delta):
     if Input.is_action_just_pressed("ui_home"):
         soul_push_back(0)
-        print(soul)
     
     if Input.is_action_just_pressed("ui_page_up"):
         soul_push_back(1)
-        print(soul)
     
     if Input.is_action_just_pressed("ui_page_down"):
         soul_push_back(2)
-        print(soul)
         
     if Input.is_action_just_pressed("ui_end"):
         soul_push_back(3)
-        print(soul)
     
     if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_down") and Input.is_action_pressed("ui_left"):
         rotation += fmod((spin_speed * delta), 360)
@@ -89,8 +137,6 @@ func _process(delta):
 
         if Input.is_action_pressed("ui_left"):
             move(3, delta)
-    
-    speed_sum = speed[0] + speed[1] + speed[2] + speed[3]
     
     for i in len(speed):
         if speed[i] <= 0:

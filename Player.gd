230ef906f18d456efec333e2f36255e5a2197eab
Sean Allen <angstyloop@gmail.com>
2023-05-28 02:42:16 -0700

    finished "four winds" basic movement

--- Player.gd
+++ Player.gd
@@ -1,14 +1,16 @@
 extends Area2D
 
 var speed
-var speed_sum
+var spin_speed = 12
 var direction
+var speed_sum
 var soul
 const len_soul = 4
 const initial_speed = 1
 const speed_limit = 10000
 const initial_acceleration = 5000
 const acceleration_growth = 100
+const directions = [Vector2.UP, Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT]
 var drag
 const stopping_speed = 250
 
@@ -30,47 +32,63 @@ func soul_push_back(soul_index):
     emit_signal("soul_switch", soul)
         
 func move(direction_index, delta):
-        if speed[direction_index] == 0:
-            speed[direction_index] = initial_speed
-        
-        speed[direction_index]  += initial_acceleration * exp(acceleration_growth * delta) * delta
-        
-        if speed[direction_index]  > speed_limit:
-            speed[direction_index]  = speed_limit
+    if (Vector2.ZERO - direction).length() < 0.000001:
+        direction = directions[direction_index]
+    else:
+        direction = (direction + directions[direction_index]) / sqrt(2)
+    if speed[direction_index] == 0:
+        speed[direction_index] = initial_speed
+    
+    speed[direction_index]  += initial_acceleration * exp(acceleration_growth * delta) * delta
+    
+    if speed[direction_index]  > speed_limit:
+        speed[direction_index]  = speed_limit
     
 func _process(delta):
-  
-    if Input.is_action_pressed("ui_up"):
-        move(0, delta)
-    
-    if Input.is_action_pressed("ui_right"):
-        move(1, delta)
-    
-    if Input.is_action_pressed("ui_down"):
-        move(2, delta)
-
-    if Input.is_action_pressed("ui_left"):
-        move(3, delta)
-    
     if Input.is_action_just_pressed("ui_home"):
         soul_push_back(0)
         print(soul)
-        pass
     
     if Input.is_action_just_pressed("ui_page_up"):
         soul_push_back(1)
         print(soul)
-        pass
     
     if Input.is_action_just_pressed("ui_page_down"):
         soul_push_back(2)
         print(soul)
-        pass
         
     if Input.is_action_just_pressed("ui_end"):
         soul_push_back(3)
         print(soul)
-        pass
+    
+    if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_down") and Input.is_action_pressed("ui_left"):
+        rotation += fmod((spin_speed * delta), 360)
+        return
+    
+    elif Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_down"):
+        move(1, delta)
+    
+    elif Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_down") and Input.is_action_pressed("ui_left"):
+        move(2, delta)
+        
+    elif Input.is_action_pressed("ui_down") and Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_up"):
+        move(3, delta)
+    
+    elif Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_right"):
+        move(0, delta)
+    
+    else:        
+        if Input.is_action_pressed("ui_up"):
+            move(0, delta)
+        
+        if Input.is_action_pressed("ui_right"):
+            move(1, delta)
+        
+        if Input.is_action_pressed("ui_down"):
+            move(2, delta)
+
+        if Input.is_action_pressed("ui_left"):
+            move(3, delta)
     
     speed_sum = speed[0] + speed[1] + speed[2] + speed[3]
     
@@ -83,4 +101,4 @@ func _process(delta):
             speed[i] -= drag * stopping_speed * delta
             
     position += (Vector2.UP * speed[0] + Vector2.RIGHT * speed[1] + Vector2.DOWN * speed[2] + Vector2.LEFT * speed[3]) * delta
-
+    rotation = -direction.angle_to(directions[2])
